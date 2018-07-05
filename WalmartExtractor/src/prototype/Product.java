b406208ad483a060fd40b6c5dba49454bafad672
rrowell1979 <40679881+rrowell1979@users.noreply.github.com>
2018-07-05 14:22:41 -0600

    Add files via upload

---
+++ WalmartExtractor/src/prototype/Product.java
@@ -0,0 +1,45 @@
+
+public class Product {
+
+	String title;
+	Double price;
+	String description;
+	String id;
+	String retailer;
+	String tid;
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public Double getPrice()
+	{
+		return price;
+	}
+	
+	public String getdescription()
+	{
+		return description;
+	}
+	
+	public String getId()
+	{
+		return id;
+	}
+	
+	public String getRetailer()
+	{
+		return retailer;
+	}
+	
+	public String getTid()
+	{
+		return tid;
+	}
+	
+	public void setTitle()
+	{
+		
+	}
+}
