bfbc3e9601054d5f92958a6102bd7c8d3080c250
rrowell1979 <40679881+rrowell1979@users.noreply.github.com>
2018-07-26 18:10:42 -0600

    Update class

--- WalmartExtractor2/src/prototype/Database.java
+++ WalmartExtractor2/src/prototype/Database.java
@@ -1,24 +1,60 @@
 package prototype;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Database {
-//	
-//	try{
-//		// 1. Get a connection to database
-//		Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webextractor", "username", "password");
-//		// 2. Create a statment
-//		Statement myStmt = myConn.createStatement();
-//		// 3. Execue SQL query
-//		ResultSet myResult = myStmt.executeQuery("select * from webextractor");
-//		while (myResult.next()){
-//			System.out.println(myResult.getString("set_id") + " | "+ myResult.getString("retailer") + " | " + myResult.getString("title") + " | "+ myResult.getString("description") + " | "+ myResult.getString("price"));
-//		}
-//	}
-//	catch (Exception exc){
-//			exc.printStackTrace();;
-//	}
+	
+	public static Connection getConnection()throws Exception{
+		try{
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/webextractor";
+			String username = "root";
+			String password = "persius1979";
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(url, username, password);
+			return conn;
+			}catch(Exception e){System.out.println(e);}
+		return null;
+	}
+	
+	public static void getData()
+	{
+	//Connect to Database
+			try{
+				// 1. Create a statment
+				Connection myConn = getConnection();
+				Statement myStmt = myConn.createStatement();
+				// 2. Execue SQL query
+				ResultSet myResult = myStmt.executeQuery("select * from webextractor");
+				while (myResult.next()){
+					System.out.println(myResult.getString("id") + " | "+ myResult.getString("tld") + " | " + myResult.getString("retailer") + " | "+ myResult.getString("title") + " | "+ myResult.getString("description")+" | "+ myResult.getString("price"));
+				}
+			}
+			catch (Exception exc){
+					exc.printStackTrace();
+			}
+	}
+	
+	public static void insertData() throws Exception{
+		
+		Product testing = new Product(null, null, null, null, null, null);
+
+		try{
+			Connection myConn = getConnection();
+			PreparedStatement posted = myConn.prepareStatement("INSERT INTO webextractor (id, tld, retailer, title, description, price) VALUES ('"+testing.getId()+"','"+testing.getTld()+"','"+testing.getRetailer()+"','"+testing.getTitle()+"','"+testing.getDescription()+"', '"+testing.getPrice()+"')");
+			
+			posted.executeUpdate();
+			}catch(Exception e){System.out.println(e);}
+		
+			finally{
+				System.out.println("Insert Completerd");
+		}
+	}
+	
 }
