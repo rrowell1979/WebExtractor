1b026213a00f0f527da013f9f248a9a8f7b45599
rober <robertjesseewhite@gmail.com>
2018-07-26 18:02:27 -0600

    Scheduler

--- WalmartExtractor2/src/prototype/Database.java
+++ WalmartExtractor2/src/prototype/Database.java
@@ -6,19 +6,24 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Database {
-//	
-//	try{
-//		// 1. Get a connection to database
-//		Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webextractor", "username", "password");
-//		// 2. Create a statment
-//		Statement myStmt = myConn.createStatement();
-//		// 3. Execue SQL query
-//		ResultSet myResult = myStmt.executeQuery("select * from webextractor");
-//		while (myResult.next()){
-//			System.out.println(myResult.getString("set_id") + " | "+ myResult.getString("retailer") + " | " + myResult.getString("title") + " | "+ myResult.getString("description") + " | "+ myResult.getString("price"));
-//		}
-//	}
-//	catch (Exception exc){
-//			exc.printStackTrace();;
-//	}
+	
+	public void printTable()
+	{
+		try{
+			// 1. Get a connection to database
+			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webextractor", "username", "password");
+			// 2. Create a statment
+			Statement myStmt = myConn.createStatement();
+			// 3. Execue SQL query
+			ResultSet myResult = myStmt.executeQuery("select * from webextractor");
+			while (myResult.next())
+			{
+				System.out.println(myResult.getString("set_id") + " | "+ myResult.getString("retailer") + " | " + myResult.getString("title") + " | "+ myResult.getString("description") + " | "+ myResult.getString("price"));
+			}
+		}
+		catch (Exception exc)
+		{
+				exc.printStackTrace();
+		}
+	}
 }
--- WalmartExtractor2/src/prototype/Scheduler.java
+++ WalmartExtractor2/src/prototype/Scheduler.java
@@ -1,5 +1,34 @@
 package prototype;
 
-public class Scheduler {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class Scheduler extends TimerTask{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+
+	    //the Date and time at which you want to execute
+	    DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    Calendar today = Calendar.getInstance();
+//	    Date date = new Date(today.set(Calendar.HOUR_OF_DAY, value);, month, date, hrs, min)
+
+	    //Now create the time and schedule it
+	    Timer timer = new Timer();
+
+	    //Use this if you want to execute it once
+//	    timer.schedule(new Scheduler(), date);
+
+	    //Use this if you want to execute it repeatedly
+	    //int period = 10000;//10secs
+	    //timer.schedule(new MyTimeTask(), date, period );
+	}
 }
