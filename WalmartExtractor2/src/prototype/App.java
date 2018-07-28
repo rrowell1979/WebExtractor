5d010fcee89172e53d46c622b7d125ca5a7cb14d
rober <robertjesseewhite@gmail.com>
2018-07-28 11:30:13 -0600

    Updates

--- WalmartExtractor2/src/prototype/App.java
+++ WalmartExtractor2/src/prototype/App.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
+import com.mysql.*;
 
 //Russell Rowell, Robert White, Karan Singh, Kevin Wu
 //CSIS 2450
@@ -30,8 +31,23 @@ public class App
     	Product product;
     	Extractor ext = new Extractor();
         product = ext.parseProductAttributes("54594234","walmart","com");
+        
+        Product[] productArray;
+        String[] ids = new String[3];
+        ids[0] = "14869676";
+        ids[1] = "26832646";
+        ids[2] = "952592653";
+        productArray = ext.parseProductAttributes(ids, "walmart", "com");
+        
     	try {
 			db.insertData(product);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	try {
+			db.insertData(productArray);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
